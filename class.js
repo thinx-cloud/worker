683c4364d861a15abd3baef1b511f135421b37a5
Matej Sychra <suculent@me.com>
2022-03-24 23:15:52 +0100

    manual merge

--- class.js
+++ class.js
@@ -1,24 +1,13 @@
-if (typeof(process.env.SQREEN_TOKEN) !== "undefined") {
-    require('sqreen');
-}
-
-if (typeof(process.env.ROLLBAR_TOKEN) !== "undefined") {
-    var Rollbar = require('rollbar');
-    new Rollbar({
-        accessToken: process.env.ROLLBAR_TOKEN,
-        handleUncaughtExceptions: true,
-        handleUnhandledRejections: true
-    });
-}
-
 const exec = require("child_process");
 const version = require('./package.json').version;
 const io = require('socket.io-client');
 const fs = require("fs-extra");
 const chmodr = require('chmodr');
-class Worker {
+
+module.exports = class Worker {
 
     constructor(build_server) {
+        console.log(`${new Date().getTime()} -= THiNX Cloud Build Worker ${version} =-`);
         this.client_id = null;
         this.socket = io(
             build_server,
@@ -27,9 +16,9 @@ class Worker {
                 rejectUnauthorized: false // because the certificate on other side has different CNAME than 'api'; sufficient for internal communication
             }
         );
-
-        console.log(`${new Date().getTime()} -= THiNX Cloud Build Worker ${version} =-`);
+        console.log(`${new Date().getTime()} setting up socket...`);
         this.setupSocket(this.socket);
+        console.log(`${new Date().getTime()} socket setup completed...`);
         this.socket_id = null;
         this.running = false;
     }
@@ -48,7 +37,7 @@ class Worker {
 
     validateJob(sock, job) {
 
-        if (typeof(job.cmd) === "undefined") {
+        if (typeof (job.cmd) === "undefined") {
             this.failJob(sock, job, "Missing command");
             return false;
         }
@@ -63,31 +52,30 @@ class Worker {
             return false;
         }
 
-        if (typeof(job.build_id) === "undefined") {
+        if (typeof (job.build_id) === "undefined") {
             this.failJob(sock, job, "Missing build_id");
             return false;
         }
 
-        if (typeof(job.udid) === "undefined") {
+        if (typeof (job.udid) === "undefined") {
             this.failJob(sock, job, "Missing udid");
             return false;
         }
 
-        if (typeof(process.env.WORKER_SECRET) !== "undefined") {
-            if (typeof(job.secret) === "undefined") {
+        if ((typeof (process.env.WORKER_SECRET) === "undefined") || (process.env.WORKER_SECRET === null)) {
+            if (typeof (job.secret) === "undefined") {
                 this.failJob(sock, job, "Missing job secret");
                 return false;
-            } else {
-                if (job.secret === null) {
-                    console.log(`${new Date().getTime()} Warning, JOB SECRET NULL! This will be error soon. ${job}`);
-                    return false;
-                } else {
-                    if (job.secret.indexOf(process.env.WORKER_SECRET) !== 0) {
-                        this.failJob(sock, job, "Invalid job authentication");
-                        return false;
-                    } 
-                }
             }
+            if (job.secret === null) {
+                console.log(`${new Date().getTime()} Warning, JOB SECRET NULL! This will be error soon. ${job}`);
+                return false;
+            }
+        }
+
+        if (job.secret.indexOf(process.env.WORKER_SECRET) !== 0) {
+            this.failJob(sock, job, "Invalid job authentication");
+            return false;
         }
 
         return true;
@@ -99,6 +87,13 @@ class Worker {
             console.log(`${new Date().getTime()} Setting worker to running...`);
             this.running = true;
             this.runShell(job.cmd, job.owner, job.build_id, job.udid, job.path, sock);
+            setTimeout(() => {
+                if (this.running) {
+                    console.log("[error] worker timed out after 1h, stopping.");
+                    this.running = false;
+                    this.failJob(sock, job, "worker_time_out");
+                }
+            }, 3600 * 1000);
         } else {
             console.log(`${new Date().getTime()} [critical] Job validation failed on this worker. Developer error, or attack attempt. No shell will be run.`);
         }
@@ -106,16 +101,16 @@ class Worker {
 
     isBuildIDValid(build_id) {
         // build id may include [:alnum:] and - only
-        var pattern = new RegExp(/^([a-zA-Z0-9-]+)$/);
+        var pattern = /^([a-zA-Z0-9-]+)$/;
         return (pattern.test(build_id));
     }
 
     isArgumentSafe(CMD) {
-        var pattern = new RegExp("(?![;&]+)");
+        var pattern = /(?![;&]+)/;
         return pattern.test(CMD);
     }
 
-    runShell(CMD, owner, build_id, udid, path, socket) {
+    runShell(CMD, owner, build_id, udid, path, socket, exit_callback) {
 
         // Prevent injection through git, branch
 
@@ -138,7 +133,7 @@ class Worker {
         let tomes = CMD.split(" ");
 
         for (let tome in tomes) {
-            if ( (tome.indexOf("--git=") !== -1) || (tome.indexOf("--branch=") !== -1)) {
+            if ((tome.indexOf("--git=") !== -1) || (tome.indexOf("--branch=") !== -1)) {
                 if (!this.isArgumentSafe(tome)) {
                     console.log(`[error] Tome ${tome} invalid, suspected command injection, exiting!`);
                     return;
@@ -146,25 +141,25 @@ class Worker {
             }
         }
 
-        console.log(`[info] worker runShell command: ${tomes}`);
+        console.log(`ℹ️ [info] worker runShell command: ${tomes}`);
         let command = tomes.join(" ");
-        
+
         // deepcode ignore CommandInjection: this is expected functionality, risk should be accepted.
         let shell = exec.spawn(command, { shell: true }); // lgtm [js/command-line-injection]
         let build_start = new Date().getTime();
 
-		shell.stdout.on("data", (data) => {
-			var string = data.toString();
+        shell.stdout.on("data", (data) => {
+            var string = data.toString();
             var logline = string;
-            
-            logline = logline.replace(/\r\r/g, '');
-			logline = logline.replace(/\n\n/g, '');
 
-			if (logline.length > 1) {
+            logline = logline.replace(/\r\r/g, '');
+            logline = logline.replace(/\n\n/g, '');
+
+            if (logline.length > 1) {
                 console.log(logline);
 
-				if (logline.indexOf("JOB-RESULT") !== -1) {
-                    
+                if (logline.indexOf("JOB-RESULT") !== -1) {
+
                     // parses "[86ad8d90-46e8-11eb-a48a-b59a7e739f77] »» JOB-RESULT:" {...
                     let start_pos = logline.indexOf("{");
                     let annotation_string = logline.substr(start_pos);
@@ -172,38 +167,38 @@ class Worker {
                     let status_object = {
                         udid: udid,
                         state: "Failed",
-                        build_id: build_id, 
+                        build_id: build_id,
                         owner: owner
                     };
 
                     try {
                         let annotation_json = JSON.parse(annotation_string);
                         status_object = annotation_json;
-                        
+
                     } catch (e) {
                         console.log(`[error] Annotation status in '${annotation_string}' not parsed.`);
                     }
 
                     let elapsed_hr;
-                    let build_time = (new Date().getTime() - build_start)/1000; // to seconds
+                    let build_time = (new Date().getTime() - build_start) / 1000; // to seconds
                     if (build_time < 60) {
                         elapsed_hr = build_time + " seconds";
                     } else {
-                        let minutes = Math.floor(build_time/60);
+                        let minutes = Math.floor(build_time / 60);
                         let seconds = Math.floor(build_time % 60);
                         elapsed_hr = minutes + " minutes " + seconds + " seconds";
                     }
 
-                    console.log(`[info] BUILD TIME: ${elapsed_hr}`);
+                    console.log(`ℹ️ [info] BUILD TIME: ${elapsed_hr}`);
 
                     status_object.elapsed = build_time;
                     status_object.elapsed_hr = elapsed_hr;
-                    
+
                     status_object.completed = true;
                     socket.emit('job-status', status_object); // should be called job-result everywhere, always indiates completion
 
                     // calculate build time
-				}
+                }
             }
 
             // Something must write to build_path/build.log where the file is tailed from to websocket...
@@ -226,25 +221,25 @@ class Worker {
             });
 
             socket.emit('log', logline + "\n");
-            
+
         }); // end shell on out data
-        
+
         var dstring = "unknown";
 
-		shell.stderr.on("data", (data) => {
-			let ddstring = data.toString();
-			if (ddstring.indexOf("fatal:") !== -1) {
+        shell.stderr.on("data", (data) => {
+            let ddstring = data.toString();
+            if (ddstring.indexOf("fatal:") !== -1) {
                 this.running = false;
                 socket.emit('job-status', {
                     udid: udid,
-                    build_id: build_id, 
+                    build_id: build_id,
                     state: "Failed",
                     reason: ddstring
                 });
-			}
-		}); // end shell on error data
+            }
+        }); // end shell on error data
 
-		shell.on("exit", (code) => {
+        shell.on("exit", (code) => {
 
             console.log(`[OID:${owner}] [BUILD_COMPLETED] with code ${code}`);
             this.running = false;
@@ -252,38 +247,44 @@ class Worker {
             if (code > 0) {
                 socket.emit('job-status', {
                     udid: udid,
-                    build_id: build_id, 
+                    build_id: build_id,
                     state: "Failed",
                     reason: dstring
                 });
             }
 
+            // exit_callback is only in test
+            if (typeof (exit_callback) !== "undefined") {
+                exit_callback();
+                return;
+            }
+
             const close_underlying_connection = true; // should be true, having it false does not help failing builds
             socket.disconnect(close_underlying_connection);
-            
-		}); // end shell on exit
-	}
+
+        }); // end shell on exit
+    }
 
     setupSocket(socket) {
-        
+
         // Connectivity Events
 
-        socket.on('connect', () => { 
+        socket.on('connect', () => {
             socket.emit('register', { status: "Hello from BuildWorker.", id: this.socket_id, running: this.running });
         });
 
-        socket.on('disconnect', () => { 
+        socket.on('disconnect', () => {
             console.log(`${new Date().getTime()} » Worker socket disconnected.`);
         });
 
         // either by directly modifying the `auth` attribute
         socket.on("connect_error", () => {
-            if ((typeof(process.env.WORKER_SECRET) !== "undefined")) {
-                if (typeof(socket.auth) !== "undefined") {
+            if ((typeof (process.env.WORKER_SECRET) !== "undefined")) {
+                if (typeof (socket.auth) !== "undefined") {
                     socket.auth.token = process.env.WORKER_SECRET;
                     console.log(`${new Date().getTime()} connect_error attempt to resolve using WORKER_SECRET`);
                 }
-                setTimeout(function(){
+                setTimeout(function () {
                     socket.connect();
                 }, 10000);
             }
@@ -291,7 +292,7 @@ class Worker {
 
         // Business Logic Events
 
-        socket.on('client id', (data) => { 
+        socket.on('client id', (data) => {
             if (this.client_id === null) {
                 console.log(`${new Date().getTime()} » Worker received initial client id: ${data}`);
             } else {
@@ -300,18 +301,18 @@ class Worker {
             this.client_id = data;
         });
 
-        socket.on('job', (data) => { 
-            if (this.running == true) {
+        socket.on('job', (data) => {
+            if (this.running) {
                 console.log(`${new Date().getTime()} This worker is already running... passing job ${data}`);
                 return;
             }
-            // Prevent path traversal by rejecting insane values
-            if (typeof(job.path) !== "undefined" && job.path.indexOf("..") !== -1) {
+            // Prevent path traversal by rejecting insane values (WTF?)
+            if (typeof (job.path) !== "undefined" && job.path.indexOf("..") !== -1) {
                 console.log(`${new Date().getTime()} [error] Invalid path (no path traversal allowed).`);
                 return;
             }
             console.log(new Date().getTime(), `» Worker has new job:`, data);
-            if (typeof(data.mock) === "undefined" || data.mock !== true) {
+            if (typeof (data.mock) === "undefined" || data.mock !== true) {
                 this.client_id = data;
                 this.runJob(socket, data);
                 this.running = false;
@@ -331,30 +332,8 @@ class Worker {
             console.log(`${new Date().getTime()} [info] » Skipping poll cron (job still running and did not timed out).`);
         }
     }
-}
 
-// Init phase off-class
-
-let srv = process.env.THINX_SERVER;
-let worker = null;
-
-if (typeof(srv) === "undefined" || srv === null) {
-    console.log(`${new Date().getTime()} [critical] THINX_SERVER environment variable must be defined in order to build firmware with proper backend binding.`);
-    process.exit(1);
-} else {
-    // fix missing http if defined in env file just like api:3000
-    if (srv.indexOf("http") == -1) {
-        srv = "http://" + srv;
+    close() {
+        this.socket.close();
     }
-    console.log(`${new Date().getTime()} [info] » Starting build worker against ${srv}`);
-
-    try {
-        worker = new Worker(srv);
-    } catch (e) {
-        // in test environment there is a test worker running on additional port 3001 as well...
-        console.log(`Caught exception ${e}`);
-        let srv2 = srv1.replace(":3000", ":3001");
-        // eslint-disable-next-line no-unused-vars
-        worker = new Worker(srv2);
-    }
-}
+};
