0b08f877e4dcb0db38fc8aa3441522bf4b2df052
Matej Sychra <suculent@me.com>
2022-04-10 13:08:48 +0200

    another terrible bugfix

--- worker.js
+++ worker.js
@@ -1,12 +1,15 @@
-let s = null;
+let s = null; // Sqreen
+let r = null; // Rollbar
 
-if (typeof(process.env.SQREEN_TOKEN) !== "undefined") && (process.env.SQREEN_TOKEN !== null) {
+function exists(x) {
+    return ((typeof(x) === "undefined") || (x === null)) ? false : true;
+}
+
+if (exists(process.env.SQREEN_TOKEN)) {
     s = require('sqreen');
 }
 
-let r = null;
-
-if (typeof(process.env.ROLLBAR_ACCESS_TOKEN) !== "undefined") && (process.env.ROLLBAR_ACCESS_TOKEN !== null) {
+if (exists(process.env.ROLLBAR_ACCESS_TOKEN)) {
     var Rollbar = require('rollbar');
     r = new Rollbar({
         accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
@@ -22,21 +25,12 @@ let Worker = require("./class.js");
 let srv = process.env.THINX_SERVER;
 let worker = null;
 
-function exists(x) {
-    if ((typeof(x) === "undefined") || (x === null)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-if (typeof(srv) === "undefined" || srv === null) {
+if (exists(srv)) {
     console.log(`${new Date().getTime()} [critical] THINX_SERVER environment variable must be defined in order to build firmware with proper backend binding.`);
     process.exit(1);
-} else {
-    console.log(`${new Date().getTime()} [info] » Starting build worker against ${srv}`);
+} 
 
-    worker = new Worker(srv);
+console.log(`${new Date().getTime()} [info] » Starting build worker against ${srv}`);
+worker = new Worker(srv);
 
-    if (exists(r)) r.info(["Worker started with server"+srv]);
-}
+if (exists(r)) r.info(["Worker started with server"+srv]);
