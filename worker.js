34d5579b2f2d526b8b304e89032eae2c2664735e
Matej Sychra <suculent@me.com>
2022-04-10 13:52:44 +0200

    added missing function to failing worker

--- worker.js
+++ worker.js
@@ -5,6 +5,10 @@ function exists(x) {
     return ((typeof(x) === "undefined") || (x === null)) ? false : true;
 }
 
+function undef(x) {
+    return !exists(x);
+}
+
 if (exists(process.env.SQREEN_TOKEN)) {
     s = require('sqreen');
 }
@@ -23,7 +27,6 @@ let Worker = require("./class.js");
 // Init phase off-class
 
 let srv = process.env.THINX_SERVER;
-let worker = null;
 
 if (undef(srv)) {
     console.log(`${new Date().getTime()} [critical] THINX_SERVER environment variable must be defined in order to build firmware with proper backend binding.`);
@@ -31,6 +34,6 @@ if (undef(srv)) {
 } 
 
 console.log(`${new Date().getTime()} [info] » Starting build worker against ${srv}`);
-worker = new Worker(srv);
+const worker = new Worker(srv);
 
 if (exists(r)) r.info(["Worker started with server"+srv]);
